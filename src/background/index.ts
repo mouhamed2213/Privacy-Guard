b9c71d124e27371b6e9d13eec982a85238267f59
mouhamed2213 <Mouhamedbk1002@gmail.com>
2025-09-24 22:55:31 +0000

    config manifest / SW : setup manifest and service wk

--- src/background/index.ts
+++ src/background/index.ts
@@ -18,3 +18,9 @@ chrome.runtime.onInstalled.addListener(() => {
     console.log("Règle de test pour example.com installée.");
   });
 });
+
+/*
+Description : C'est le cerveau de votre extension. Ce script s'exécute en arrière-plan, 
+invisible pour l'utilisateur. C'est lui qui contient la logique principale.
+Rôle actuel : Il écoute l'événement d'installation de l'extension (onInstalled) et,
+ à ce moment-là, il demande au navigateur de mettre en place une règle simple pour bloquer le site example.com.*/ 
