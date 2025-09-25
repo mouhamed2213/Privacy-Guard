737ad9af5e1ae800bcd3805c0bf49a163da98387
mouhamed2213 <Mouhamedbk1002@gmail.com>
2025-09-25 00:37:50 +0000

    Feat : First test

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,3 +1,6 @@
+/** Notre code actuel (src/background/index.ts) est censé bloquer toute tentative de navigation 
+ * vers le site example.com. Nous allons vérifier si c'est bien le cas.*/
+
 console.log("Privacy Guard Service Worker a démarré !");
 
 chrome.runtime.onInstalled.addListener(() => {
@@ -19,8 +22,10 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 });
 
+
 /*
 Description : C'est le cerveau de votre extension. Ce script s'exécute en arrière-plan, 
 invisible pour l'utilisateur. C'est lui qui contient la logique principale.
+
 Rôle actuel : Il écoute l'événement d'installation de l'extension (onInstalled) et,
  à ce moment-là, il demande au navigateur de mettre en place une règle simple pour bloquer le site example.com.*/ 
